a72709aecffa5eeb663bc8466b356b26a786ec1c
aldompe95 <aldompe95@gmail.com>
2017-02-23 17:29:33 -0600

    little changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('#changeNav');
+  var offset = startchange.offset();
+  if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start >= offset.top) {
+        $(".navbar-default").css('background-color', '#333');
+      }else{
+        $(".navbar-default").css('background-color', 'rgba(0, 0, 0, 0.24)');
+      }
+   });
+  }
 	$(window).on('resize', function(){
 		var width=$(window).width();
 		console.log(width);
